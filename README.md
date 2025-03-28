239a00b2f977672b103d3cee978122af5db27ea9
Anirudh Nambiar <anirudhnambiar15@gmail.com>
2025-03-29 01:35:05 +0530

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# Multilevel-Feedback-Queue
+# **Multilevel Feedback Queue (MLFQ) Scheduler** 
+
+## **Problem Statement**
+Implemented a Multi-level Feedback Queue Scheduling algorithm which simulates how processes are scheduled in a MLFQ in Operating systems.
+
+### **MLFQ Scheduling Details**
+An operating system implements **multilevel feedback queue scheduling** with **four queues**:
+
+1. **Q0 (Highest Priority)** → Uses **Round-Robin (RR)** with a time slice of **5ms**.  
+2. **Q1** → Uses **First-Come, First-Served (FCFS)** scheduling.  
+3. **Q2** → Uses **Priority Scheduling**, where priority is taken as the inverse of the CPU time needed by the jobs.  
+4. **Q3 (Lowest Priority)** → Uses **Shortest Job First (SJF)** scheduling.
+
+### **Rules of Execution**
+- Each job is **allocated 5ms in each queue** before moving to a lower-level queue.  
+- Once **all processes finish execution in a queue,** the next queue starts processing.  
+- If jobs are **still incomplete** after **Q3**, they are **returned to Q0** and the cycle repeats until completion.  
+- **All scheduling algorithms are assumed to be preemptive**.  
+- The goal is to **calculate the average turnaround time** for the given workload.
+
+
+### **Sample Workload**
+| Process | Total CPU Time Needed |
+|---------|-----------------------|
+| A       | 300 ms                |
+| B       | 150 ms                |
+| C       | 250 ms                |
+| D       | 350 ms                |
+| E       | 450 ms                |
+
+---
+
+## **How to Run the Code**
+### **1. Install a C++ Compiler**
+The program is written in **C++**. You need a **C++ compiler** to run the code.
+
+#### **On Windows**
+You can install **MinGW** (which includes `g++`):
+```sh
+winget install -e --id MSYS2.MSYS2
+```
+Then, open **MSYS2** and install `gcc`:
+```sh
+pacman -S mingw-w64-ucrt-x86_64-gcc
+```
+Verify installation:
+```sh
+g++ --version
+```
+
+Alternatively, if you have **Visual Studio** installed, you can compile the code using:
+```sh
+cl /EHsc filename.cpp
+```
+
+#### **On macOS**
+```sh
+xcode-select --install
+```
+
+---
+
+## **User Input Format**
+When prompted, enter the number of processes and their CPU times:
+
+Example:
+```
+Enter number of Processes: 5
+Enter Process Name and BurstTime: A 300
+Enter Process Name and BurstTime: B 150
+Enter Process Name and BurstTime: C 250
+Enter Process Name and BurstTime: D 350
+Enter Process Name and BurstTime: E 450
+```
+
+---
+
+## **Output**
+The program will display the **turnaround time** for each process and the **average turnaround time**:
+
+Example:
+```
+Scheduling Started!
+Turnaround Time of Process A is 850 ms
+Turnaround Time of Process B is 400 ms
+Turnaround Time of Process C is 700 ms
+Turnaround Time of Process D is 950 ms
+Turnaround Time of Process E is 1200 ms
+Average Turnaround Time: 820 ms
+```
+
+---
---
+++ mlfq.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <pthread.h>
+#include <queue>
+#include <mutex>
+#include <algorithm>
+
+#define TIME_SLICE 5
+
+using namespace std;
+
+struct Process{
+    string name;
+    int remainingTime;
+    int completionTime;
+    int burstTime;
+
+    Process(string n, int time){
+        name = n;
+        burstTime = time;
+        remainingTime = time;
+        completionTime = 0;
+    }
+};
+
+struct customComparator {
+    bool operator() (Process &p1 , Process &p2){
+        return p1.burstTime > p2.burstTime ;
+    }
+};
+
+void roundRobin(queue<Process> &q0, queue<Process> &q1, queue<Process> &q4, int &currTime ){
+    while(!q0.empty()){
+
+        Process p = q0.front();
+        q0.pop();
+        int executionTime = min(TIME_SLICE,p.remainingTime);
+        currTime += executionTime;
+        p.remainingTime -= executionTime;
+
+        if(p.remainingTime > 0){
+            q1.push(p);
+        } else{
+            p.completionTime = currTime ;
+            q4.push(p);
+        }
+    }
+}
+
+void firstComeFirstServe(queue<Process> &q1, priority_queue<Process, vector<Process>, customComparator> &q2, queue<Process> &q4, int &currTime){
+    while(!q1.empty()){
+        Process p = q1.front();
+        q1.pop();
+        int executionTime = min(TIME_SLICE,p.remainingTime);
+        currTime += executionTime;
+        p.remainingTime -= executionTime;
+
+        if(p.remainingTime > 0){
+            q2.push(p);
+        } else{
+            p.completionTime = currTime;
+            q4.push(p);
+        }
+    }
+}
+
+void prioritySchedule(priority_queue<Process, vector<Process>, customComparator> &q2, priority_queue<Process, vector<Process>, customComparator> &q3, queue<Process> &q4, int &currTime){
+    while(!q2.empty()){
+        Process p = q2.top();
+        q2.pop();
+        int executionTime = min(TIME_SLICE,p.remainingTime);
+        currTime += executionTime;
+        p.remainingTime -= executionTime;
+
+        if(p.remainingTime > 0){
+            q3.push(p);
+        } else {
+            p.completionTime = currTime;
+            q4.push(p);
+        }
+    }
+}
+
+void shortestJobFirst(priority_queue<Process, vector<Process>, customComparator> &q3, queue<Process> &q0, queue<Process> &q4, int &currTime){
+    while(!q3.empty()){
+        Process p = q3.top();
+        q3.pop();
+        int executionTime = min(TIME_SLICE,p.remainingTime);
+        currTime += executionTime;
+        p.remainingTime -= executionTime;
+
+        if(p.remainingTime > 0){
+            q0.push(p);
+        } else {
+            p.completionTime = currTime;
+            q4.push(p);
+        }
+    }
+}
+
+int main(){
+    int totalProcesses = 0;
+    cout << "Enter no. of Processes: " ;
+    cin >> totalProcesses;
+    int currTime = 0;
+
+//Queue q0 = RR, q1 = FCFS, q2 = Priority Scheduling, q3 = Shortest Job First, q4 = Last queue(Calculation)
+// Two queues in each funtion, so that after one quanta is over it will go to the next queue(also in RR)
+queue<Process> q0,q1, q4;
+priority_queue<Process, vector<Process>, customComparator> q2, q3;
+
+    for(int i=0;i<totalProcesses;++i){
+        cout << "Enter Process Name and BurstTime: ";
+        string n = "";
+        int t = 0;
+        cin >> n >> t ;
+        Process p(n,t);
+        q0.push(p);
+    }
+    cout << endl ;
+
+    cout << "Scheduling Started!" << endl ;
+    cout << endl ;
+
+    while(!q0.empty() || !q1.empty() || !q2.empty() || !q3.empty()){
+        roundRobin(q0, q1, q4,currTime);
+        firstComeFirstServe(q1,q2, q4,currTime);
+        prioritySchedule(q2,q3, q4,currTime);
+        shortestJobFirst(q3,q0,q4,currTime);
+    }
+
+    int totalTime =0;
+
+    while(!q4.empty()){
+        Process p = q4.front();
+        totalTime += p.completionTime ;
+        cout << "Turnaround Time of Process " << p.name << " is " << p.completionTime << " ms" << endl ;
+        q4.pop();
+    }
+    cout << endl ;
+    cout << "Average Turnaround Time is " << (float)totalTime/totalProcesses << "ms" << endl ;
+    return 0 ;
+}
